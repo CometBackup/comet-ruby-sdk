af5a94eaaec4c12152bdc4c312021a1e15ab002f
Ben Frengley <bfrengley@cometbackup.com>
2024-05-14 14:56:27 +1200

    comet: sync with comet 24.3.7

--- lib/comet/definitions.rb
+++ lib/comet/definitions.rb
@@ -7,13 +7,13 @@
 
 module Comet
 
-  APPLICATION_VERSION = '24.3.6'
+  APPLICATION_VERSION = '24.3.7'
 
   APPLICATION_VERSION_MAJOR = 24
 
   APPLICATION_VERSION_MINOR = 3
 
-  APPLICATION_VERSION_REVISION = 6
+  APPLICATION_VERSION_REVISION = 7
 
   # AutoRetentionLevel: The system will automatically choose how often to run an automatic Retention Pass after each backup job.
   BACKUPJOBAUTORETENTION_AUTOMATIC = 0
